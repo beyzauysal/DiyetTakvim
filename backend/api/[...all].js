889edff77a49fcffbf2e1c58e4cb8f2c4ec8fa38
Beyza Uysal <uysalbeyza27@gmail.com>
2026-04-01 01:45:03 +0300

    Son değişiklikler

---
+++ backend/api/[...all].js
@@ -0,0 +1,10 @@
+const { app, ensureMongoConnected } = require("../app");
+
+module.exports = async (req, res) => {
+  try {
+    await ensureMongoConnected();
+  } catch (err) {
+    console.error("Mongo bağlantı hatası:", err?.message || err);
+  }
+  return app(req, res);
+};
